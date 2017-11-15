52d39d55848a5e06b78bb767a94027e82950dc8b
Dario <fussinatto@gmail.com>
2017-11-15 21:31:44 +0100

    1st working state

--- src/App.js
+++ src/App.js
@@ -5,30 +5,50 @@ import './App.css';
 class App extends Component {
   constructor() {
     super()
-    this.renderCanvas = this.renderCanvas.bind(this)
+    this.state = {
+      text: "Hello World"
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  renderCanvas () {
-    const ctx = new C2S(200, 100);
+  renderCanvas() {
+    const text = this.state.text;
+    const ctx = new C2S(1000, 500);
     ctx.fillStyle = "red";
     ctx.font = "48px Pacifico";
-    ctx.fillText('Hello world', 10, 50);
+    ctx.fillText(`${text}`, 10, 50);
 
-    var mySerializedSVG = ctx.getSerializedSvg(); //true here, if you need to convert named to numbered entities.
-
+    // var mySerializedSVG = ctx.getSerializedSvg(); //true here, if you need to convert named to numbered entities.
     //If you really need to you can access the shadow inline SVG created by calling:
     var svg = ctx.getSvg();
     return svg
-  } 
+  }
 
-  componentDidMount() {
-    const svg = this.renderCanvas();
+  handleChange(e) {
+    const text = e.target.value
+    this.setState({ text })
+  }
+
+  renderSvg() {
+    const svg = this.renderCanvas()
+    this.refs.svgContainer.innerHTML = ''
     this.refs.svgContainer.appendChild(svg)
   }
-  
+
+  componentDidUpdate () {
+    this.renderSvg();
+  }
+
+  componentDidMount() {
+    this.renderSvg();
+  }
+
   render() {
     return (
-      <div ref="svgContainer"></div>
+      <div>
+        <div ref="svgContainer"></div>
+        <input type="text" value={this.state.text} onChange={this.handleChange.bind(this)} />
+      </div>
     );
   }
 }
