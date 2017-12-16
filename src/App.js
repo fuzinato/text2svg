eadc5de4b5f7c93502be5153161aadf0a18c1f69
Dario Fuzinato <fussinatto@gmail.com>
2017-12-16 17:06:11 +0100

    add icons to menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { ChromePicker } from 'react-color'
-import {  Button, Icon } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 import WebFont from 'webfontloader'
 import C2S from './vendor/canvas2svg'
 import FontFamilySelect from './FontFamilySelect'
@@ -25,13 +25,13 @@ class App extends Component {
       fontSize: 48,
       isPickerVisible: false
     }
-    
+
     this._debounceTextChange = debounce(this._debounceTextChange, 400);
   }
 
   // Private
   _renderCanvas() {
-    const { text, color, fontSize, fontFamily, fontStyle, fontWeight} = this.state;
+    const { text, color, fontSize, fontFamily, fontStyle, fontWeight } = this.state;
     const ctx = new C2S(1000, 200)
 
     ctx.fillStyle = `${color}`
@@ -48,8 +48,8 @@ class App extends Component {
   handleTextChange(e) {
     this._debounceTextChange(e.target.value)
   }
-  
-  _debounceTextChange(text){
+
+  _debounceTextChange(text) {
     this.setState({ text })
   }
 
@@ -85,11 +85,11 @@ class App extends Component {
     const flags = /(\d+)?(\D+)?/g.exec(fontVariant)
     const fontWeightRgx = flags[1]
     const fontStyleRgx = flags[2]
-    let fontWeight, 
-        fontStyle = "normal", 
-        fvd = "";
+    let fontWeight,
+      fontStyle = "normal",
+      fvd = "";
 
-    if (fontWeightRgx && fontWeightRgx !== "regular"){
+    if (fontWeightRgx && fontWeightRgx !== "regular") {
       fontWeight = parseInt(fontWeightRgx, 10)
     } else {
       fontWeight = "400"
@@ -112,13 +112,13 @@ class App extends Component {
         families: [`${this.state.fontFamily}:${fvd}`]
       }
     });
-    this.setState({fontVariant})
+    this.setState({ fontVariant })
   }
 
   renderSvg() {
     const svg = this._renderCanvas()
-    this.refs.svgContainer.innerHTML = ''
-    this.refs.svgContainer.appendChild(svg)
+    this.svgContainer.innerHTML = ''
+    this.svgContainer.appendChild(svg)
   }
 
   downloadSVGFile() {
@@ -131,7 +131,7 @@ class App extends Component {
 
   showColorPicker() {
     console.log(this.state.isPickerVisible)
-    this.setState({isPickerVisible: !this.state.isPickerVisible})
+    this.setState({ isPickerVisible: !this.state.isPickerVisible })
   }
 
   // Lifecycle hooks
@@ -149,11 +149,11 @@ class App extends Component {
       return { text: variant, value: variant }
     });
     return (
-      <div>
-        <div ref="svgContainer"></div>
+      <div className="app">
+        <div ref={(el) => { this.svgContainer = el }}></div>
         {/* Text input */}
         <Menu />
-        <input 
+        <input
           type="text"
           placeholder="Enter Text"
           defaultValue={this.state.text}
@@ -163,25 +163,30 @@ class App extends Component {
           color={this.state.color}
           className="color-picker"
           className={this.state.isPickerVisible ? "is-visible" : "is-hidden"}
-          onChangeComplete={this.handleColorChange.bind(this)}/> 
+          onChangeComplete={this.handleColorChange.bind(this)} />
         {/* Font Size */}
-        <input 
+        <input
           type="number"
           placeholder='Font Size'
-          value={this.state.fontSize} 
+          value={this.state.fontSize}
           onChange={this.handleFontSizeChange.bind(this)} />
         <Button icon color="pink" onClick={this.showColorPicker.bind(this)} ><Icon name='tint' /></Button>
         <Button icon color="blue" onClick={this.downloadSVGFile.bind(this)} ><Icon name='download' /></Button>
         <Button icon color="teal" onClick={this.showSVGCode.bind(this)}><Icon name='code' /></Button>
         {/* Font Family */}
-        <FontFamilySelect 
+        <FontFamilySelect
           changeFont={this.changeFont.bind(this)}
-          fontFamily={this.state.fontFamily}/>
+          fontFamily={this.state.fontFamily} />
         {/* Font Style */}
         <FontStyleSelect
           variants={this.state.variants}
           fontVariant={this.state.fontVariant}
-          changeStyle={this.changeStyle.bind(this)}/>
+          changeStyle={this.changeStyle.bind(this)} />
+
+
+        <Button icon="github" />
+        <Button icon="twitter" />
+        <Button icon="share alternate" color="pink" />
 
       </div>
     );
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,54 @@
 import React from 'react'
-import {  Button, Icon } from 'semantic-ui-react'
+import { Popup, Button, Icon } from 'semantic-ui-react'
 
 const Menu = () => {
   return (
-    <div>
-      <Icon name="tint" />
-      <Button icon><Icon name='text cursor' /></Button>
-      <Button icon><Icon name='text height' /></Button>
-      <Button icon><Icon name='download' /></Button>
-      <Button icon><Icon name='code' /></Button>
-      <Button icon><Icon name='font' /></Button>
-      <Button icon><Icon name='bold' /></Button>
-      <Button icon color="pink"><Icon name='tint' /></Button>
-      <Button icon color="pink"><Icon name='share alternate' /></Button>
+    <div class="menu">
+      <Popup
+        trigger={<Button icon='compose'  size='huge' />}
+        content='Add users to your feed'
+        position='top left'
+      />
+      <Popup
+        trigger={<Button icon='text height' size='huge' />}
+        content='Add users to your feed'
+        position='top center'
+      />
+      <Popup
+      trigger={<Button icon><Icon name='tint' color='purple' size='large'/></Button>}
+      content='Add users to your feed'
+      position='top center'
+    />
+      <Popup
+        trigger={<Button icon='code' size='huge' />}
+        content='Add users to your feed'
+        position='top right'
+      />
+
+      <Popup
+        trigger={<Button icon='book' size='huge' />}
+        content='Add users to your feed'
+        position='bottom left'
+      />
+
+      <Popup
+        trigger={<Button icon='italic' size='huge' />}
+        content='Add users to your feed'
+        position='bottom center'
+      />
+
+      <Popup
+        trigger={<Button icon='crop' size='huge' />}
+        content='Add users to your feed'
+        position='bottom center'
+      />
+      <Popup
+        trigger={<Button icon='download' size='huge' />}
+        content='Add users to your feed'
+        position='bottom right'
+      />
+    
+
     </div>
   )
 }
